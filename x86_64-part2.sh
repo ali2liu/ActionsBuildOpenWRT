7f98dd7f6a7c18c625a4f7f4591c097161a1e724
ali2liu <ali2liu@gmail.com>
2024-02-16 21:48:45 +0800

    Update x86_64-part2.sh

--- x86_64-part2.sh
+++ x86_64-part2.sh
@@ -9,3 +9,8 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 #git clone --depth=1 --filter=blob:none --sparse https://github.com/vernesong/OpenClash.git
 #cd OpenClash
 #git sparse-checkout set luci-app-openclash
+
+# Custom patch
+cd openwrt
+cp $GITHUB_WORKSPACE/912-skipping-wrong-addresses-causes-service-to-exit.patch ./build_dir/target-x86_64_musl/dnsmasq-full/dnsmasq-2.88/
+patch -p1 < ./build_dir/target-x86_64_musl/dnsmasq-full/dnsmasq-2.88/912-skipping-wrong-addresses-causes-service-to-exit.patch
